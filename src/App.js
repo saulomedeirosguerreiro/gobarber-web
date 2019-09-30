83b39a5feed523e2157c285aba270136bbbb0dea
Saulo Medeiros <saulo.medeiros.guerreiro@gmail.com>
2019-09-30 10:45:17 -0300

    Creating custom routes and page layouts

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import history from './services/history';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
